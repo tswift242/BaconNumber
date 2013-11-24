2794374f7952ae16ea69d4229f9f8dfbfa7b5998
Thomas Swift <tswift@u.rochester.edu>
2013-11-23 23:01:33 -0500

    added credits to pickle_method.py

--- bndlearner/pickle_method.py
+++ bndlearner/pickle_method.py
@@ -1,6 +1,12 @@
 from copy_reg import pickle
 from types import MethodType
 
+"""Module for picking and unpickling instance methods of classes, as well as
+for registering these functions with pickle.
+Taken Steven Bethard's solution at: 
+http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods#edit2155350
+"""
+
 # registers methods for pickling and unpickling a class instance method
 def register_pickle_method():
 	pickle(MethodType, _pickle_method, _unpickle_method)
